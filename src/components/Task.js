93f21f7d8f1deec2bc6215461bba25798a6a59d4
PictureElement <marios.sofokleous@yandex.com>
2021-10-21 15:58:14 +0300

    Create ActiveButton and CompletedButton components. Conditionally render one of them based on task active state.

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,5 +1,7 @@
 import db from '../firebase';
 import { doc, deleteDoc } from "firebase/firestore";
+import ActiveButton from './ActiveButton';
+import CompletedButton from './CompletedButton';
 
 function Task(props) {
   // Delete document from db
@@ -7,12 +9,19 @@ function Task(props) {
     //await deleteDoc(doc(db, "tasks", id));
     alert('delete document');
   }
+  
+  let button;
+
+  // Select button component based on task active state
+  if (props.task.is_active) {
+    button = <ActiveButton onClick={e => deleteTask(props.task.id)} />
+  } else {
+    button = <CompletedButton onClick={e => deleteTask(props.task.id)} />
+  }
 
   return (
     <article className="todo-item">
-      <button onClick={e => deleteTask(props.task.id)} className="todo-item__button" type="button">
-        <svg className="todo-item__icon" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px"><path d="M0 0h24v24H0z" fill="none"></path><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 15l-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z"></path></svg>
-      </button>
+      {button}
       <div className="todo-item__title">
         <span>{props.task.title}</span>
       </div>
