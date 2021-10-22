87154f2d0f24988b270b90ddbc224e05e2e40452
Marios Sofokleous <marios.sofokleous@yandex.com>
2021-10-22 14:11:31 +0300

    Implement dark mode toggle.

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,24 +1,53 @@
+import { useState, useEffect } from 'react'; // Hooks
 import LightThemeToggle from './LightThemeToggle';
 import DarkThemeToggle from './DarkThemeToggle';
 
 function Hero(props) {
 
-    let toggle;
-
-    if(false) {
-        toggle = <LightThemeToggle />
+  const [theme, setTheme] = useState();
+  
+  useEffect(() => {
+    // Set theme to user preference
+    if (localStorage.getItem('theme')) {
+      if (localStorage.getItem('theme') === 'theme-light') {
+        changeThemeToLight();
+      } else {
+        changeThemeToDark();
+      }
+    // Otherwise set to default 
     } else {
-        toggle = <DarkThemeToggle />
+      changeThemeToLight();
     }
+  }, [])
 
-    return (
-        <section className="hero">
-            <div className="container d-flex justify-content-between align-items-center">
-                <h1 className="hero__title h2">{props.title}</h1>
-                {toggle}
-            </div>
-        </section>
-    )
+  const changeThemeToLight = () => {
+    document.documentElement.classList.remove('theme-dark');
+    localStorage.setItem('theme', 'theme-light');
+    setTheme('theme-light');
+  }
+
+  const changeThemeToDark = () => {
+    document.documentElement.classList.add('theme-dark');
+    localStorage.setItem('theme', 'theme-dark');
+    setTheme('theme-dark');
+  }
+
+  let toggle;
+
+  if(theme === 'theme-light') {
+    toggle = <LightThemeToggle onClick={changeThemeToDark} />
+  } else {
+    toggle = <DarkThemeToggle onClick={changeThemeToLight} />
+  }
+
+  return (
+    <section className="hero">
+      <div className="container d-flex justify-content-between align-items-center">
+        <h1 className="hero__title h2">{props.title}</h1>
+        {toggle}
+      </div>
+    </section>
+  )
 }
 
 export default Hero
