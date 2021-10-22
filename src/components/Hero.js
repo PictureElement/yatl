3d8f6071edcbfc871375726c0ffad9045acf45db
Marios Sofokleous <marios.sofokleous@yandex.com>
2021-10-22 12:36:59 +0300

    Create LightThemeToggle and DarkThemeToggle components.

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,11 +1,21 @@
-import Toggle from './Toggle';
+import LightThemeToggle from './LightThemeToggle';
+import DarkThemeToggle from './DarkThemeToggle';
 
 function Hero(props) {
+
+    let toggle;
+
+    if(false) {
+        toggle = <LightThemeToggle />
+    } else {
+        toggle = <DarkThemeToggle />
+    }
+
     return (
         <section className="hero">
             <div className="container d-flex justify-content-between align-items-center">
                 <h1 className="hero__title h2">{props.title}</h1>
-                <Toggle />
+                {toggle}
             </div>
         </section>
     )
