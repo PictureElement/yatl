200a4c0fb8d895a9748704fc7c419cdd58b2ac26
Marios Sofokleous <marios.sofokleous@yandex.com>
2021-10-27 22:58:40 +0300

    Add form, submit button, and text input  on Editing template. Add specific styles to editing mode. Handle form submit on Editing mode.

--- src/components/Task.js
+++ src/components/Task.js
@@ -9,6 +9,21 @@ function Task(props) {
   // 'isEditing' state
   const [isEditing, setEditing] = useState(false);
 
+  // 'newTitle' state
+  const [newTitle, setNewTitle] = useState('');
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    // Don't add empty tasks
+    if (newTitle) {
+      props.editTask(props.task.id, newTitle);
+      // Clear new title
+      setNewTitle('');
+      // Return to viewing mode
+      setEditing(false)
+    }
+  }
+
   let button;
 
   // Select button component based on task state
@@ -49,28 +64,30 @@ function Task(props) {
   // Editing template
   const editTemplate = (
     <li className="todo-item">
-      <div className="todo-item__group-left form-check">
-        <input
-          className="form-check__input"
-          id={props.task.id}
-          type="checkbox"
-          defaultChecked={props.task.is_active}
-          onChange={() => props.updateStatus(props.task.id)}
-        />
-        <label className="todo-item__label form-check__label" htmlFor={props.task.id}>
-          {props.task.title}
-        </label>
-      </div>
-      <div className="todo-item__group-right">
-        <button onClick={() => setEditing(false)} className="todo-item__button todo-item__button_primary mr-2" type="button">
-          <svg className="todo-item__icon todo-item__icon_primary" xmlns="http://www.w3.org/2000/svg" height="18px" viewBox="0 0 24 24" width="18px"><path d="M0 0h24v24H0V0z" fill="none" opacity=".87"/><path d="M12 2C6.47 2 2 6.47 2 12s4.47 10 10 10 10-4.47 10-10S17.53 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm3.59-13L12 10.59 8.41 7 7 8.41 10.59 12 7 15.59 8.41 17 12 13.41 15.59 17 17 15.59 13.41 12 17 8.41z"/></svg>
-          <span className="visually-hidden">Cancel</span>
-        </button>
-        <button onClick={props.onClick} className="todo-item__button todo-item__button_success" type="button">
-          <svg className="todo-item__icon todo-item__icon_success" xmlns="http://www.w3.org/2000/svg" height="18px" viewBox="0 0 24 24" width="18px"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M17 3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V7l-4-4zm2 16H5V5h11.17L19 7.83V19zm-7-7c-1.66 0-3 1.34-3 3s1.34 3 3 3 3-1.34 3-3-1.34-3-3-3zM6 6h9v4H6z"/></svg>
-          <span className="visually-hidden">Save</span>
-        </button>
-      </div>
+      <form class="d-flex justify-content-between align-items-center w-100" onSubmit={handleSubmit}>
+        <div className="todo-item__group-left form-text">
+          <label className="visually-hidden" htmlFor={props.task.id}>
+            New title for {props.task.title}
+          </label>
+          <input
+            className="form-text__input mr-2"
+            id={props.task.id}
+            type="text"
+            onChange={(e) => setNewTitle(e.target.value)}
+            value={newTitle}
+          />
+        </div>
+        <div className="todo-item__group-right">
+          <button onClick={() => setEditing(false)} className="todo-item__button todo-item__button_primary mr-2" type="button">
+            <svg className="todo-item__icon todo-item__icon_primary" xmlns="http://www.w3.org/2000/svg" height="18px" viewBox="0 0 24 24" width="18px"><path d="M0 0h24v24H0V0z" fill="none" opacity=".87"/><path d="M12 2C6.47 2 2 6.47 2 12s4.47 10 10 10 10-4.47 10-10S17.53 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm3.59-13L12 10.59 8.41 7 7 8.41 10.59 12 7 15.59 8.41 17 12 13.41 15.59 17 17 15.59 13.41 12 17 8.41z"/></svg>
+            <span className="visually-hidden">Cancel</span>
+          </button>
+          <button onClick={props.onClick} className="todo-item__button todo-item__button_success" type="submit">
+            <svg className="todo-item__icon todo-item__icon_success" xmlns="http://www.w3.org/2000/svg" height="18px" viewBox="0 0 24 24" width="18px"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M17 3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V7l-4-4zm2 16H5V5h11.17L19 7.83V19zm-7-7c-1.66 0-3 1.34-3 3s1.34 3 3 3 3-1.34 3-3-1.34-3-3-3zM6 6h9v4H6z"/></svg>
+            <span className="visually-hidden">Save</span>
+          </button>
+        </div>
+      </form>
     </li>
   );
 
